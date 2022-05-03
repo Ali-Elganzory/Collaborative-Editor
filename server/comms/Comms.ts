58bfa1f752ac833aff6590106c0a2805eae179af
Ali Elganzory <alielganzory@hotmail.com>
2022-05-03 06:48:10 +0200

    Doc-commented Comms and Synchronizer interfaces + Revised the interfaces.
    - Doc-commented Comms and Synchronizer interfaces.
    - Revised the API of both interfaces.

--- server/comms/Comms.ts
+++ server/comms/Comms.ts
@@ -1,6 +1,37 @@
-import sio from "socket.io";
+import { Diff } from "diff-match-patch";
+
+import Synchronizer, { ClientSentEditsCallback } from "../synchronizer/Synchronizer";
 
 
+/**
+ * Responsible for all server communications.
+ */
 export default interface Comms {
+
+    /**
+     * Registers a synchronizer to resolve user's incoming edits
+     * and update the user with other collaborators' edits.
+     * 
+     * @param synchronizer Any implementation of [Synchronizer] interface.
+     */
+    registerSynchronizer(synchronizer: Synchronizer): void;
+
+    /**
+     * Sends an array of [Diff]s to a user in a specific document
+     * session.
+     * 
+     * @param uid user id
+     * @param documentId document id
+     * @param edits edits to send to the user
+     */
+    sendDocumentEditsToUser(uid: string, documentId: string, edits: ReadonlyArray<Diff>): Promise<void>;
+
+    /**
+     * Global event callback for any received edits from 
+     * client side.
+     * 
+     * @param callback to call on edits
+     */
+    onClientSentEdits(callback: ClientSentEditsCallback): void;
 
 }
--- server/synchronizer/Synchronizer.ts
+++ server/synchronizer/Synchronizer.ts
@@ -2,20 +2,35 @@ import { Diff } from "diff-match-patch";
 
 
 export type SendEditsToClientCallback = (edits: Diff[]) => void
-export type ClientSentEditsCallback = (edits: Diff[], sendEditsToClient: SendEditsToClientCallback) => void
+export type ClientSentEditsCallback = (edits: Diff[]) => void
 /**
- * a tuple of the initial document content and a callback for future user edits.
+ * a tuple of the initial document content and a callback for future user edits, respectively.
  */
 export type ClientEnteredSessionResponse = [string, ClientSentEditsCallback];
 
 
-interface Synchronizer {
+export default interface Synchronizer {
 
     readonly documents: ReadonlyArray<Document>;
 
-    onClientEnteredSession(uid: string, documentId: bigint): false | ClientEnteredSessionResponse;
-    onClientLeftSession(uid: string): boolean;
+    /**
+     * call it whenever a user starts a session 
+     * to get initial document content as well 
+     * as a callback for user edits.
+     * 
+     * @param uid user id
+     * @param documentId session's document id
+     */
+    clientEnteredSession(uid: string, documentId: bigint, sendEditsToClient: SendEditsToClientCallback): false | ClientEnteredSessionResponse;
 
-    _onClientSentEdits(uid: string, edits: Diff[],): ReadonlyArray<Diff>;
+    /**
+     * call it whenever a users closes a session
+     * to clean his reserved resources.
+     * 
+     * @param uid user id
+     */
+    clientLeftSession(uid: string): boolean;
+
+    _clientSentEdits(uid: string, edits: Diff[],): ReadonlyArray<Diff>;
 
 }
